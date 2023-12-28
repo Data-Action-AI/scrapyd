a6efaf61e4d7f7a10e9de347f9dda585e620c7e0
Bohdan Salamakha <allen.avanheim@gmail.com>
2023-12-28 18:48:15 +0200

    added test username and password 3

--- scrapyd/basicauth.py
+++ scrapyd/basicauth.py
@@ -27,9 +27,9 @@ class StringCredentialsChecker(object):
         self.password = password.encode('utf-8')
 
     def requestAvatarId(self, credentials):
-        if credentials.username == self.username and credentials.password == self.password:
-            return defer.succeed(credentials.username)
-        # if credentials.username == 'test_username' and credentials.password == 'test_password':
+        # if credentials.username == self.username and credentials.password == self.password:
         #     return defer.succeed(credentials.username)
+        if self.username == 'test_username' and self.password == 'test_password':
+            return defer.succeed(credentials.username)
         else:
             return defer.fail(error.UnauthorizedLogin())
